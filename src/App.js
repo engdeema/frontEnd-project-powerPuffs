a6fba9b0fc857099411fa0e8b03e43cb60a0b24c
amnah5 <amooon_96@hotmail.com>
2021-11-09 16:28:01 +0300

    css

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import { Routes, Route } from "react-router-dom";
 import CategoryList from "./components/CategoryList";
 // Components
 import NavBar from "./components/NavBar";
+import Appetizars from "./components/Appetizars";
 
 function App() {
   return (
     <>
       <NavBar />
-
       <Routes>
         <Route path="/" element={<CategoryList />} />
+      </Routes>
+      <Routes>
+        <Route path="/Appatizer" element={<Appetizars />} />
       </Routes>
     </>
   );
